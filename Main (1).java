a66da04ca369626826b797bbd9fe73cdb2dff021
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-03-14 17:43:21 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+class Employee1
+{
+    int empid;
+    String name;
+    int salary;
+    String DOB;
+    double HRA()
+    {
+    double hra=(salary*20)/100;
+    return hra;
+    }
+    }
+    class Manager extends Employee
+    {
+    double DA()
+    {
+    double da=(salary*10)/100;
+    return da;
+   }
+}
+
+class Main
+{
+    public static void main(String args[])
+    {
+    Manager lalitha=new Manager();
+    lalitha.salary=20000;
+    System.out.println(lalitha.salary);
+    double hr=lalitha.HRA();
+    System.out.println(hr);
+    double d=lalitha.DA();
+    System.out.println(d);
+    double grosssalary=lalitha.salary+d+hr;
+    System.out.println(grosssalary);
+    }
+}
+
+
+    
