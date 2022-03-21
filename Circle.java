36d42de0a93aa9b05fa36ab223a7a5aaaceb317e
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-03-21 17:47:47 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,9 @@
+//Circle class inside of the package
+package Shape;
+public class Circle
+{
+    public void draw()
+    {
+        System.out.println("Circle");
+    }
+}
---
+++ Square.java
@@ -0,0 +1,8 @@
+//Square class inside of the package
+package Shape;
+public class Square
+{
+    public void draw(){
+        System.out.println("Square");
+    }
+}
