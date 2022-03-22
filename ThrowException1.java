8e5d385a4a531025dfd02b5ff08d48398b7ff2a2
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-03-22 18:25:08 +0530

    Add files via upload

---
+++ ThrowException1.java
@@ -0,0 +1,28 @@
+class ThrowException1
+{
+    static void drivingLicence(int age,String bike)
+    {
+        if(age>18&&bike.equals("yes"))
+        {
+            System.out.println("Eligible");
+        }
+        else
+        {
+            throw new StringIndexOutOfBoundsException("Not Eligible");
+        }
+    }
+    public static void main (String args[])
+    {
+        try {
+                drivingLicence(17,"yes");
+        } 
+        catch(Exception e) 
+        {
+                System.out.println(e);
+        }       
+        finally 
+        {
+            
+        }
+    }        
+}            
