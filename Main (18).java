ed86ddc5ac6a0ad57a57f4360d12fae79dee9733
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-03-28 11:40:26 +0530

    Add files via upload

---
+++ Main (18).java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+    public static void main(String[] args) {
+    String name=new String();
+    name="SaiKiran";
+    System.out.println(name);
+        for(int i=0;i<name.length();i++)
+        {
+            System.out.println(name.charAt(i));
+        }
+
+    }
+}
