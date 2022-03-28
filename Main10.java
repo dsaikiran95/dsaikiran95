aca45216e6027d635f620c59bcbd188d720b442f
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-03-28 10:30:43 +0530

    Update and rename Main (10).java to Main10.java

--- Main10.java
+++ Main10.java
@@ -6,7 +6,7 @@ C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, S
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-class Main {
+class Main10 {
     static void main()
     {
         System.out.println("Fake Man");
