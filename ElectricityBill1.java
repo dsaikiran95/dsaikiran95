af5d734638b90fca974b36a3fcd2fd015755ebcb
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-03-29 15:32:44 +0530

    Update and rename ElectricityBill (1).java to ElectricityBill1.java

--- ElectricityBill1.java
+++ ElectricityBill1.java
@@ -3,7 +3,7 @@
     
       
 import java.util.*;
-public class ElectricityBill {
+public class ElectricityBill1 {
 
     public static void main(String args[]) {
     // create object for Scanner class
